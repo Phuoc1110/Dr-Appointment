1208ecc913db10158fa71e5da9e40ca3c6b57aec
Phuoc1110 <phuoc11102004@gmail.com>
2025-03-06 18:26:05 +0700

    Banner_2

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
+import { navigate, useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+
+    const navigate = useNavigate()
+
   return (
-    <div>
+    <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
         {/* Bên trái */}
-        <div>
-            <p>Đặt Lịch Khám Với</p>
-            <p>Hơn 100 Bác Sĩ Đáng Tin Cậy</p>
+        <div className='flex-1 py-8 sm:py-16 lg:py-24 lg:pl-5 '>
+            <div className='text-xl sm:text-2x1 md:text-3xl lg:text-5xl font-semibol text-white'>
+                <p>Đặt Lịch Khám Với</p>
+                <p className='mt-4'>Hơn 100 Bác Sĩ Đáng Tin Cậy</p>
+            </div>
+            <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Đăng Ký</button>
         </div>
-        <button>Đăng Ký</button>
+        
 
         {/* Bên phải */}
-        <div>
-            <img src={assets.appointment_img} alt="" />
+        <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
+            <img className='w-full absolute bottom-0 right-0 max-w-md' src={assets.appointment_img} alt="" />
         </div>
 
 
