6ca63f4878e9cb7ee58cf53ba8830efc24467761
Phuoc1110 <phuoc11102004@gmail.com>
2025-03-07 22:04:10 +0700

    Fix Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,17 @@ import {assets} from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my_10 mt-40 text_sm'>
             {/* LEFT */}
             <div>
-                <img src={assets.logo} alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt blanditiis officiis repellat iusto quo amet quos veniam neque molestiae. Et aliquid nobis hic unde eveniet. Necessitatibus recusandae deserunt aliquid autem.</p>
+                <img className='mb-5 w-40' src={assets.logo} alt="" />
+                <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt blanditiis officiis repellat iusto quo amet quos veniam neque molestiae. Et aliquid nobis hic unde eveniet. Necessitatibus recusandae deserunt aliquid autem.</p>
             </div>
             {/* CENTER */}
             <div>
-                <p>COMPANY</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>Home</li>
                     <li>About us</li>
                     <li>Contact us</li>
@@ -22,16 +22,17 @@ const Footer = () => {
             </div>
             {/* RIGHT */}
             <div>
-                <p>GET IN TOUCH</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+84 335 435 578</li>
                     <li>phuoc11102004@gmail.com</li>
                 </ul>
             </div>
             {/* COPYRIGHT */}
-            <div>
-                <p>Copyright 2024@ Prescripto - All right reserved</p>
-            </div>
+        </div>
+        <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2024@ Prescripto - All right reserved</p>
         </div>
     </div>
   )
