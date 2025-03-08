37ccda4906a57ebd4680b95678dee857caa937bf
Phuoc1110 <phuoc11102004@gmail.com>
2025-03-08 22:50:25 +0700

    Doctors

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
 
   const { speciality } = useParams()
-  console.log(speciality)
+
+  const { doctors } = useContext(AppContext)
 
   return (
     <div>
-        
+      <p>Brouse through the doctors specialist</p>
+      <div>
+        <div>
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+      </div>
     </div>
   )
 }
